d2b5fb83bdcb3e675574e912ae94e676f7bd58c6
OGdB <58260499+OGdB@users.noreply.github.com>
2022-01-31 19:18:57 -0500

    Dummy Test Unit set up

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 
 # Activate site (do within mysite folder)
 # python manage.py runserver
+
+# UNIT TESTS
+# Test case: Tests one part of functionality of one function
+
+# Test suite: Collection of test cases
+# Also known as test harness
+
+# Test fixture: Setup & cleanup required for a particular test
+
+# Stub: Replacement for some other component of the system; returns a canned response
